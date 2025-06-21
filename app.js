7b581ebbf0fabaf836376ffd9f7ada3e5e9836d8
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-06-21 14:17:52 +0100

    updated app.js to include user routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const pool = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 const port = 5000;
 app.get("/", (req, res) => {
@@ -11,6 +12,7 @@ app.get("/db-test", async (req, res) => {
   res.send(result.rows);
   client.release();
 });
+app.use("/users", userRoutes);
 app.listen(port, () => {
   // res.status(200).json({ message: testing });
   console.log("hello world");
