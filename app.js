2818b0e756fcab0bbee3c1792de6998802ebd766
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-06-21 06:24:38 +0100

    feat(app.js): started express server and tested db connection

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const pool = require("./config/db");
+const app = express();
+app.listen(5000, () => {
+  // res.status(200).json({ message: testing });
+  console.log("hello world");
+});
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+app.get("/db-test", async (req, res) => {
+  const client = await pool.connect();
+  const result = await client.query("SELECT NOW();");
+  res.send(result.rows);
+  client.release();
+});
